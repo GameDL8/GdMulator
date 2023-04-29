ced5658fc83cb0ac4db7d7610ec8a962e10b1d31
msuligoy <mariano.easyrpg@gmail.com>
2023-04-29 00:20:17 -0300

    Create test for BNE, and fix test for BEQ Closes #10

---
+++ addons/retro_console_emulator/tests/nes_tests/beq_bnq.gd
@@ -0,0 +1,26 @@
+extends "res://addons/retro_console_emulator/tests/base_test.gd"
+
+
+func test():
+	push_warning("Uncomment code when BQQ OpCode is implemented")
+#	test_0xf0_beq_relative_branch_if_zero_set()
+#	test_0xd0_bne_relative_branch_if_zero_clear()
+
+
+func test_0xf0_beq_relative_branch_if_zero_set():
+	var cpu = NesCPU.new()
+	cpu.load_and_run([0xa9, 0x03, 0xf0, 0x02, 0xa9, 0x05, 0xaa, 0x00])
+	assert(cpu.register_x.value == 0x03)
+	cpu.load_and_run([0xa9, 0x00, 0xf0, 0x02, 0xa9, 0x05, 0xaa, 0x00])
+	assert(cpu.register_x.value == 0x00)
+	print("test_0xf0_beq_relative_branch_if_zero_set PASSED!")
+
+
+func test_0xd0_bne_relative_branch_if_zero_clear():
+	var cpu = NesCPU.new()
+	cpu.load_and_run([0xa9, 0x03, 0xd0, 0x02, 0xa9, 0x05, 0xaa, 0x00])
+	assert(cpu.register_x.value == 0x00)
+	cpu.load_and_run([0xa9, 0x00, 0xd0, 0x02, 0xa9, 0x05, 0xaa, 0x00])
+	assert(cpu.register_x.value == 0x03)
+	print("test_0xd0_bne_relative_branch_if_zero_clear PASSED!")
+
